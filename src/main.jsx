dcb4315f87c35ebf98a280a2209e533a19b5047c
Nadija Smerechuk <nadija1smerechuk@gmail.com>
2024-09-18 09:45:42 +0300

    donefix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store";
 import App from "./components/App/App";
 import "./index.css";
@@ -10,9 +9,9 @@ import "./index.css";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate  persistor={persistor}>
+    
         <App />
-      </PersistGate>
+    
     </Provider>
   </StrictMode>
 );
